b811b14a0654032e49bbe29bd3a8f5642c978224
code with Sharvari <sharvari0822@gmail.com>
2023-06-07 10:54:38 +0530

    added changes

---
+++ connection.js
@@ -0,0 +1,17 @@
+require("dotenv").config(); 
+const { MongoClient } = require("mongodb");
+let mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+//const mongoURI = process.env.MONGODBURI;
+ const connectToMongo = async () => {
+  mongoose
+    .connect("mongodb://localhost:27017/bookmyshow", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("connection established with mongodb server online");
+    })
+    .catch((err) => {
+      console.log("error while connection", err);
+    });
+};
+
+exports.connection = connectToMongo;
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express"); 
+const app = express(); 
+const { connection } = require("./connection.js"); 
+const cors = require("cors"); 
+const bodyParser = require("body-parser"); 
+const PORT = process.env.PORT || 8080;
+ app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); 
+app.use(cors()); 
+connection(); 
+app.use("/api", require("./routes")); 
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`)); 
+module.exports = app;
